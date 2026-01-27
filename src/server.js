30e7bc71afcb875a6dc199ad13bc6ff6edc4841d
malhotrat797-commits <malhotrat797@gmail.com>
2026-01-27 23:16:26 +0530

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("FourRoots Fashion backend is running");
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(4000, () => {
+      console.log("Server running on port 4000");
+    });
+  })
+  .catch(err => console.error(err));
