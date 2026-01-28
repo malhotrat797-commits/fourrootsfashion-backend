cb40c04597714628a5539f720abe5a6804e588d5
malhotrat797-commits <malhotrat797@gmail.com>
2026-01-28 12:51:59 +0530

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -2,11 +2,16 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true },
+  email: { type: String, unique: true, sparse: true },
+  phone: { type: String, unique: true, sparse: true },
   password: String,
+  loginType: {
+    type: String,
+    enum: ["email", "google", "phone"]
+  },
   role: {
     type: String,
-    default: "customer" // customer | admin
+    default: "customer"
   }
 });
 
